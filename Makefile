78b19f3bc54e54f570e266480d2312daf4970385
Daniel Gonzalez Torralba <dangonz3@c3r1s3.42urduliz.com>
2025-09-30 18:05:28 +0200

    PREcambios transcendence anterior

--- Makefile
+++ Makefile
@@ -1,13 +1,9 @@
 up:
 	docker compose up --detach --build
-
 down:
 	docker compose down --volumes
-
 rebuild:
 	docker compose up --detach --force-recreate
-
 prune:
 	docker system prune --all --force --volumes 
-
 .PHONY: up down rebuild prune
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 // Archivo: ./backend/server.js
 const express = require('express');
 const app = express();
-// Debe coincidir con el puerto en docker-compose (4173)
-const port = 4173; 
+const port = 9000; 
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) =>
+{
   res.send('<h1>¡Hola! ¡El Backend (API) de Node.js está funcionando!</h1>');
 });
 
-app.listen(port, '0.0.0.0', () => {
+app.get('/health', (req, res) =>
+{
+  res.status(200).send('OK');
+});
+
+app.listen(port, '0.0.0.0', () =>
+{
   console.log(`Backend API escuchando en http://0.0.0.0:${port}`);
 });
