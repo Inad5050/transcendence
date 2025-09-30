bcc649a15bc9b4482ca5964c19699aef2eb264db
Eleder <eledera2@gmail.com>
2025-09-30 16:57:32 +0200

    merge dani/eleder

--- backend/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-chown -R www-data:www-data /var/www/html/database #Cambia la propiedad del directorio de la base de datos
-exec "$@" #Ejecuta el comando principal del contenedor (se lo pasamos como argumento a este script con CMD)
