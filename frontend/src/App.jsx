30511daf5791dd10a5a04aa7c97a922962c4662e
Daniel Gonzalez Torralba <dangonz3@c3r1s3.42urduliz.com>
2025-09-30 18:05:28 +0200

    PREcambios transcendence anterior

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() 
+{
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Frontend de React Funcionando</h1>
+        <p>
+          Esta página está siendo servida desde el contenedor del frontend.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
+
