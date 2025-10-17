a7561b514e06b6c1395507797b45cdff1a1dfa9e
eandres <eandres@student.42urduliz.com>
2025-10-17 13:14:54 +0200

    update constanst game pong

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -2,10 +2,10 @@ import { DifficultyLevel, DifficultyConfig } from './types';
 
 // --- CONSTANTES PARA MODO 1 VS 1 Y 2 JUGADORES ---
 export const PADDLE_LENGTH_CLASSIC = 120;
-export const PADDLE_SPEED_CLASSIC = 8;
+export const PADDLE_SPEED_CLASSIC = 10;
 
 // --- CONSTANTES PARA MODO 4 JUGADORES ---
-export const PADDLE_LENGTH_4P = 180;
+export const PADDLE_LENGTH_4P = 150;
 export const PADDLE_SPEED_4P = 12;
 
 // --- CONSTANTES GLOBALES ---
